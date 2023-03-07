44e5c2e500e103dcdf28f6b3c1bd5dad0c9a74e9
burrodecarga <edwinedwinhenriquezh@hotmail.com>
2023-03-07 14:39:09 -0400

    Layout y sidebar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import Ordenes from './components/pages/Ordenes';
+import Menu from './components/pages/Menu';
+import NuevoPlatillo from './components/pages/NuevoPlatillo';
+import Sidebar from './components/ui/Sidebar';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className='flex'>
+        <Sidebar/>
+        <div className='md:w-3/5 xl:w-4/5'>
+      <Routes>
+      <Route path="/" element={<Ordenes />}/>
+      <Route path="menu" element={<Menu/>} />
+      <Route path="nuevo-platillo" element={<NuevoPlatillo/>} />
+      </Routes>
+        </div>
     </div>
+  
+      
+    </BrowserRouter>
   );
 }
 
